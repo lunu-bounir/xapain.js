4de77462dcfa7ad152e85d6bab9d24a7c15f7f4d
Lunu Bounir <lunu.bounir@gmail.com>
2018-12-27 06:40:39 +0100

    #6

--- run.sh
+++ run.sh
@@ -1,14 +1,25 @@
 #! /bin/bash
 
-git clone https://github.com/xapian/xapian
+# install m4
+wget -O m4-1.4.9.tar.gz http://ftp.gnu.org/gnu/m4/m4-1.4.9.tar.gz
+tar -zvxf m4-1.4.9.tar.gz
+pushd m4-1.4.9
+./configure
+make
+make install
+popd
 
+# prepare
+git clone https://github.com/xapian/xapian
 ./xapian/bootstrap
 
+# make libxapian.a
 pushd xapian/xapian-core
 emconfigure ./configure CPPFLAGS='-DFLINTLOCK_USE_FLOCK' CXXFLAGS='-Oz -s USE_ZLIB=1' --disable-backend-remote
 emmake make
 popd
 
+# xapian.js (compatible with WebExtension; no wasm code)
 em++ -Oz \
   -s NO_DYNAMIC_EXECUTION=1 \
   -s USE_ZLIB=1 \
